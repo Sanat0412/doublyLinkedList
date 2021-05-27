00168ce90b8e207b9f5977858359b70d2e55ae95
Sanat Kumar Dubey <studentia312@gmail.com>
2021-05-27 20:06:25 +0530

    add insert at end method

--- src/DLinkedList.java
+++ src/DLinkedList.java
@@ -1,2 +1,26 @@
 public class DLinkedList {
+    public Node head;
+    public Node tail;
+
+    public DLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+    public void insertAtEnd(Node newNode){
+        if (head==null && tail==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.prev=tail;
+        tail.next=newNode;
+        tail=newNode;
+    }
+//    public void print(){
+//        if (head!=null){
+//        Node temp=head;
+//        while (temp!=null){
+//            System.out.println(temp.data);
+//        }
+//        }
+//    }
 }
