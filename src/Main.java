ac97e2aa4720996d692cf5138e1bbcf3ab6e851b
Sanat Kumar Dubey <studentia312@gmail.com>
2021-05-27 20:12:55 +0530

    implement Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
     DLinkedList dLinkedList=new DLinkedList();
     dLinkedList.insertAtEnd(new Node(5));
+    dLinkedList.print();
 }
 }
