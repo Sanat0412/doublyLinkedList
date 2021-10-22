39a9c5f4c9546140027c56e0da3c31c70d18b480
Kshitij Varshney <kshitijvarshney92@gmail.com>
2021-10-22 22:14:36 +0530

    add Node.java

---
+++ Node.java
@@ -0,0 +1,21 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 02-Mar-21
+ *   Time: 8:05 PM
+ *   File: Node.java
+ */
+
+package March.mar02_21_NK.two;
+
+public class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
