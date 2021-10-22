f225be879fde2438111f31920e2cc9e388da8610
Kshitij Varshney <kshitijvarshney92@gmail.com>
2021-10-22 22:17:54 +0530

    add Main.java and Add DoublyLinkedList.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 02-Mar-21
+ *   Time: 8:14 PM
+ *   File: Main.java
+ */
+
+package March.mar02_21_NK.two;
+
+public class Main {
+    public static void main(String[] args) {
+        DoublyLinkedList ll = new DoublyLinkedList();
+        ll.insertAtEnd(new Node(2));
+        ll.insertAtEnd(new Node(4));
+        ll.insertAtBegin(new Node(9));
+        ll.insertAtBegin(new Node(10));
+        ll.print();
+        ll.getSecondHighest();
+    }
+}
+
