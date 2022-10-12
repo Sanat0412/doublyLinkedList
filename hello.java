785ddcc436f497d2f92bd0b939599b5053925d43
Sachin Sharma <67816867+Sachinsharma01@users.noreply.github.com>
2022-10-12 09:39:40 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter num1: ");
+    scanf("%d", &num1);
+    printf("Enter num2: ");
+    scanf("%d", & num2);
+    sum = num1 + num2;
+    printf("Addidtion = %d", sum);
+    return 0;
+}
