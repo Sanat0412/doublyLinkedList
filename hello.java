a4de5a22414b43f5bf8b12e0429f27288a2a44ca
mangalam1234 <115993850+mangalam1234@users.noreply.github.com>
2022-10-17 17:23:37 +0530

    Update hello.java
    best way

--- hello.java
+++ hello.java
@@ -1,13 +1,40 @@
-#include <stdio.h>
+import java.util.ArrayList;
+import java.util.Scanner;
 
-int main()
-{
-    int num1, num2, sum;
-    printf("Enter num1: ");
-    scanf("%d", &num1);
-    printf("Enter num2: ");
-    scanf("%d", & num2);
-    sum = num1 + num2;
-    printf("Addidtion = %d", sum);
-    return 0;
+public class Educational_114_A_RBS {
+    static StringBuilder result = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            ArrayList<String> jhaat = new ArrayList<String>();
+            geppppp(n, 0, 0, "", jhaat);
+            for (int i = 0; i < n; i++) {
+                System.out.println(jhaat.get(i));
+            }
+        }
+    }
+
+    static void geppppp(int n, int khula, int band, String s, ArrayList<String> gaand) {
+
+        if (gaand.size() > n) {
+            return;
+        }
+
+        if (khula == n && band == n) {
+            gaand.add(s);
+            return;
+        }
+
+        if (khula < n) {
+            geppppp(n, khula + 1, band, s + "(", gaand);
+        }
+
+        if (band < khula) {
+            geppppp(n, khula, band + 1, s + ")", gaand);
+        }
+    }
+
 }
